ae493124eac5bebe2b3aed7106eb1df20a927ac9
ie-orphane <ilyasselyatime0@gmail.com>
2024-11-07 11:51:23 +0100

    cogs: add /set token <waka_token> command

--- cogs/__set__.py
+++ cogs/__set__.py
@@ -1,4 +1,5 @@
 import discord
+import requests
 from discord.ext import commands
 from models import UserData
 from utils import Social, COLOR
@@ -13,7 +14,7 @@ class Set(commands.GroupCog, name="set"):
         self.color = COLOR
 
     @discord.app_commands.command(description="set a new social link")
-    @discord.app_commands.describe(link="social link")
+    @discord.app_commands.describe(social="social name", link="social link")
     async def social(self, interaction: discord.Interaction, social: Social, link: str):
         await interaction.response.defer()
         user = UserData.read(interaction.user.id)
@@ -42,6 +43,40 @@ class Set(commands.GroupCog, name="set"):
                 color=self.color.green,
                 description=f"[{Emoji.get(social)}{social}]({new_link}) social added succefuly",
             ).set_footer(text="check your profile using /profile"),
+        )
+
+    @discord.app_commands.command(description="set a new wakatime token")
+    @discord.app_commands.describe(waka_token="the new wakatime token")
+    async def token(self, interaction: discord.Interaction, waka_token: str):
+        await interaction.response.defer()
+        user = UserData.read(interaction.user.id)
+
+        response = requests.get(
+            url="https://wakatime.com/api/v1/users/current",
+            headers={
+                "Authorization": f"Basic {waka_token}",
+                "Content-Type": "application/json",
+            },
+        )
+
+        if not response.ok:
+            print(f"Error {interaction.user}: {response.status_code}, {response.text}")
+            await interaction.followup.send(
+                embed=discord.Embed(
+                    color=self.color.red,
+                    description=f"Invalid Wakatime API KEY `{waka_token}`.",
+                )
+            )
+            return
+        
+        user.token = waka_token
+        user.update()
+
+        await interaction.followup.send(
+            embed=discord.Embed(
+                color=self.color.green,
+                description=f"{interaction.user.mention}, token reseted successfully!",
+            )
         )
 
 
