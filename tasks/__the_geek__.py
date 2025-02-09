af6ac206071ac19fd309340d06ee326d0f679d6a
ie-orphane <ilyasselyatime0@gmail.com>
2025-02-09 16:00:09 +0100

    tasks: the geek
    remove the geek from the blacklist

--- tasks/__the_geek__.py
+++ tasks/__the_geek__.py
@@ -20,61 +20,58 @@ async def the_geek(bot: commands.Bot):
     geek_role = None
     black_list_role = None
 
-    # get Geek of the week and black list roles
     for role in guild.roles:
         if role.name == "The Geek":
             geek_role = role
         if role.name == "Black List":
             black_list_role = role
 
-    # if not found create new one
     if not geek_role:
         geek_role = await guild.create_role(name="The Geek", color=COLOR.yellow)
     if not black_list_role:
-        black_list_role = await guild.create_role(
-            name="Black List", color=COLOR.black
-        )
+        black_list_role = await guild.create_role(name="Black List", color=COLOR.black)
 
-    geek_id = None
-    for id in last_week.geeks:
-        black_list_member = [
-            member for member in black_list_role.members if member.id == int(id)
-        ]
+    the_geek = None
+    try:
+        the_geek_id = int(next(iter(last_week.geeks.keys())))
+        for member in guild.members:
+            if member.id == the_geek_id:
+                the_geek = member
+    except StopIteration:
+        return
 
-        if black_list_member:
-            try:
-                await black_list_member[0].remove_roles(geek_role)
-                await black_list_member[0].edit(
-                    nick=black_list_member[0].display_name.replace(" 🏆", "")
-                )
-            except Exception as e:
-                print(guild, e)
+    if the_geek is None:
+        return
 
-            continue
-
-        geek_id = int(id)
-        break
+    try:
+        if the_geek in black_list_role.members:
+            await the_geek.remove_roles(black_list_role)
+            Log.info("TheGeek", "Removed from blacklist!")
+    except Exception as e:
+        Log.error("TheGeek", f"Failed : {e}")
+        return
 
     for member in geek_role.members:
-        if member.id == geek_id:
-            return
-
-    Log.job("TheGeek", "Updating")
-
-    for geek_memeber in geek_role.members:
-        try:
-            await geek_memeber.remove_roles(geek_role)
-            await geek_memeber.edit(nick=geek_memeber.display_name.replace(" 🏆", ""))
-        except Exception as e:
-            print(guild, e)
-
-    for member in guild.members:
-        if member.id == geek_id:
+        if member.id != the_geek_id:
             try:
-                await member.add_roles(geek_role)
-                await member.edit(nick=member.display_name + " 🏆")
+                await member.remove_roles(geek_role)
+                await member.edit(
+                    nick=member.display_name.replace(" 🏆", "")
+                )
             except Exception as e:
                 Log.error("TheGeek", f"Failed : {e}")
-                return
+
+    for member in geek_role.members:
+        if member.id == the_geek_id:
+            return
+
+    Log.job("TheGeek", "Updating...")
+
+    try:
+        await the_geek.add_roles(geek_role)
+        await the_geek.edit(nick=the_geek.display_name + " 🏆")
+    except Exception as e:
+        Log.error("TheGeek", f"Failed : {e}")
+        return
 
     Log.job("TheGeek", "Updated!")
