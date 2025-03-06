e434db8262c76eaa517203e43a4ceb03e9656e1b
ie-orphane <ilyasselyatime0@gmail.com>
2025-03-06 15:36:07 +0000

    cogs: add '/shop add' command

---
+++ cogs/__shop__.py
@@ -0,0 +1,94 @@
+import discord
+import re
+from discord.ext import commands
+from config import get_emoji
+from utils import number
+from cogs import GroupCog
+from models import ItemData
+from consts import COLOR
+
+
+@discord.app_commands.guild_only()
+class Shop(GroupCog, name="shop"):
+    @staticmethod
+    async def check(
+        interaction: discord.Interaction, check: bool, body: str, foot: str
+    ) -> bool:
+        if check:
+            await interaction.followup.send(
+                embed=discord.Embed(
+                    title="❌ Submission failed!",
+                    color=COLOR.red,
+                    description=f"{interaction.user.mention}, {body} 🚫",
+                ).set_footer(text=foot),
+                ephemeral=True,
+            )
+        return check
+
+    @discord.app_commands.command(description="Add new item in the shop.")
+    @discord.app_commands.describe(
+        _title="Item's title", _description="Item's description", price="Item's price"
+    )
+    @discord.app_commands.rename(_title="title", _description="description")
+    async def add(
+        self,
+        interaction: discord.Interaction,
+        _title: discord.app_commands.Range[str, 3, 11],
+        price: int,
+        _description: discord.app_commands.Range[str, 11, 97] = None,
+    ):
+        await interaction.response.defer(ephemeral=True)
+
+        if (user := await self.bot.user_is_unkown(interaction)) is None:
+            return
+
+        title = re.sub(r"\s+", " ", _title).strip()
+        description = (
+            re.sub(r"\s+", " ", _description).strip()
+            if _description is not None
+            else None
+        )
+
+        if (
+            await self.check(
+                interaction,
+                price <= 0,
+                f"**{price}** is an invalid price!",
+                "It must be greater than 0.",
+            )
+            or await self.check(
+                interaction,
+                11 < len(title) < 3,
+                f"**{title}** is an invalid title!",
+                "It must be between 3 and 11 characters.",
+            )
+            or await self.check(
+                interaction,
+                description is not None and 97 < len(description) < 11,
+                f"**{description}** is an invalid description!",
+                "It must be between 11 and 97 characters.",
+            )
+        ):
+            return
+
+        item = ItemData.create(title, price, user.id, description)
+
+        embed = discord.Embed(
+            color=self.color.green,
+            title="✅ Submission Successful!",
+            description=(
+                f"{interaction.user.mention}, item sent for review! 🕵️\n\n"
+                f"Item information:\n"
+                f"**ID**: `{item.id}`\n"
+                f"**Title**: {item.title}\n"
+                f"**Price**: {number(item.price)} {get_emoji('coin')}\n"
+                f"**Description**: {item.description or 'N/A'}\n"
+                f"**Author**: {interaction.user.mention}"
+            ),
+        ).set_footer(text="Please be patient. ⏳")
+
+        await interaction.followup.send(embed=embed)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Shop(bot))
