9213f0a075b26a8a7c70f62f32e42e15e2a577fd
ie-orphane <ilyasselyatime0@gmail.com>
2025-04-06 12:55:52 +0100

    contexts: new '>git log' command
    add command to show git history

---
+++ contexts/git/__log__.py
@@ -0,0 +1,43 @@
+import subprocess
+
+import discord
+from discord.ext import commands
+
+from consts import COLOR
+
+__OWNER_ONLY__ = True
+__DESCRIPTION__ = "Show Git history."
+
+
+async def __run__(*, bot: commands.Bot, message: discord.Message, number: int = 7):
+    try:
+        result = subprocess.run(
+            [
+                "git",
+                "log",
+                "--oneline",
+                f"-{number}",
+                "--color=always",
+                "--decorate=short",
+            ],
+            text=True,
+            capture_output=True,
+            check=True,
+        )
+    except Exception as e:
+        await message.reply(
+            embed=discord.Embed(
+                color=COLOR.red, title="Error", description=f"```\n{e}\n```"
+            ),
+            delete_after=11,
+        )
+        await message.delete(delay=11)
+        return
+
+    await message.reply(
+        embed=discord.Embed(
+            color=COLOR.yellow,
+            description=f"```ansi\n{result.stdout.replace("[m", "[0m")}\n```",
+        ),
+        mention_author=False,
+    )
